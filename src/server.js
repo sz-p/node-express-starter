c954dc9f0d398cb94e94f20f04bc144d2b9a6c06
sz-p <sz_p@outlook.com>
2019-12-15 09:53:59 +0800

    [feat]: helloworld router

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import appConfig from '../configs/app.config';
+import routers from './routers/routers';
+
 const { PORT } = appConfig;
 
 const app = express();
 app.listen(PORT);
+
+routers.forEach((item, index) => {
+	item(app);
+});
