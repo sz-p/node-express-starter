305d29d9c342592afa02b9444f05721312a96d08
sz-p <sz_p@outlook.com>
2020-01-14 20:36:56 +0800

    [webpack]: add two webpack plugins

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,10 +1,17 @@
 const paths = require('./paths');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const SimpleProgressPlugin = require('webpack-simple-progress-plugin');
+
 module.exports = {
-	mode: 'production',
-	target: 'node',
-	entry: paths.mainjs,
-	output: {
-		path: paths.build,
-		filename: 'bundle.[hash:8].js'
-	}
+  mode: 'production',
+  target: 'node',
+  entry: paths.mainjs,
+  output: {
+    path: paths.build,
+    filename: 'bundle.[hash:8].js'
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new SimpleProgressPlugin()
+  ]
 };
