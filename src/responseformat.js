367edd3d2169773528d50cf42960bfa7e9f6e9f1
sz-p <sz_p@outlook.com>
2020-04-24 17:40:40 +0800

    [feat]: add base error response formater

--- src/responseformat.js
+++ src/responseformat.js
@@ -1,11 +1,20 @@
 export default {
-  success: function (msg) {
-    return {
-      success: true,
-      status: 0,
-      api: msg.api,
-      information: msg.infor,
-      data: msg.data
-    }
-  }
-}
+	success: function(msg) {
+		return {
+			success: true,
+			status: 0,
+			api: msg.api,
+			information: msg.infor,
+			data: msg.data
+		};
+	},
+	error: function(msg) {
+		return {
+			success: false,
+			status: msg.status || 1,
+			api: msg.api,
+			information: msg.infor,
+			data: msg.data
+		};
+	}
+};
