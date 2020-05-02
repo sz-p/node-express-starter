e60336c8183096d2574ac834c4e3bfd2c5a8382a
sz-p <sz_p@outlook.com>
2020-05-03 00:10:35 +0800

    [webpack]: enable node global variables

--- src/server.js
+++ src/server.js
@@ -3,30 +3,30 @@ import appConfig from '../configs/app.config';
 import routers from './routers/routers';
 import https from 'https';
 import fs from 'fs';
-import bodyParser from 'body-parser';
 import addLoader from "./loader/loader";
-
+import paths from "../configs/paths";
 const { PORT, REQUESTLIMITSIZE } = appConfig;
 
+console.log(paths.datas);
 const app = express();
 
 addLoader(app);
 
 // init routers
 routers.forEach((item) => {
-	app[item[1]](item[0], (req, res) => {
-		const requestData = Object.assign(req.query, req.body);
-		item[2](requestData, req, res).then((d) => res.send(d)).catch((d) => res.send(d));
-	});
+  app[item[1]](item[0], (req, res) => {
+    const requestData = Object.assign(req.query, req.body);
+    item[2](requestData, req, res).then((d) => res.send(d)).catch((d) => res.send(d));
+  });
 });
 
 // start listen
 if (appConfig.HTTPS.enable) {
-	const options = {
-		key: fs.readFileSync(appDirectory + appConfig.HTTPS.keyPath),
-		cert: fs.readFileSync(appDirectory + appConfig.HTTPS.certPath)
-	};
-	https.createServer(options, app).listen(PORT);
+  const options = {
+    key: fs.readFileSync(appDirectory + appConfig.HTTPS.keyPath),
+    cert: fs.readFileSync(appDirectory + appConfig.HTTPS.certPath)
+  };
+  https.createServer(options, app).listen(PORT);
 } else {
-	app.listen(PORT);
+  app.listen(PORT);
 }
