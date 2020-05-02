074cadd664186096e28ff8d224f26e0d466320b1
sz-p <sz_p@outlook.com>
2020-05-02 23:29:56 +0800

    [fix]: fix loader oath error

--- src/loader/loader.js
+++ src/loader/loader.js
@@ -1,3 +1,7 @@
+import appConfig from '../../configs/app.config';
+import bodyParser from 'body-parser';
+const { REQUESTLIMITSIZE } = appConfig;
+
 export default function (app) {
   app.use(bodyParser.json({ limit: REQUESTLIMITSIZE }));
   app.use(bodyParser.urlencoded({ limit: REQUESTLIMITSIZE, extended: false }));
