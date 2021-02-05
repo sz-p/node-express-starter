23920ead010ed56ec48c5a2d47369093bf154b32
sz-p <sz_p@outlook.com>
2021-02-05 18:08:15 +0800

    feat: log start message when start

--- scripts/start.js
+++ scripts/start.js
@@ -1,1 +1,10 @@
-require("../src/server.js");
+const config = require("../configs/app.config").default;
+try {
+  require("../src/server.js");
+  console.log(
+    "\x1B[32m%s\x1B[0m",
+    `Server running at http://localhost:${config.PORT}`
+  );
+} catch (e) {
+  console.log("\x1B[31m%s\x1B[0m", "Server startup failed");
+}
