8b1db04984e9c070dbed6d4ca398c20784a96a97
sz-p <SZ_P@outlook.com>
2021-12-23 01:20:28 +0800

    fix: 修复打包后无法正确识别config路径的问题

--- configs/paths.js
+++ configs/paths.js
@@ -1,11 +1,12 @@
 // paths 文件 导出了一堆有用的路径信息
 const path = require("path");
 let resolveApp = undefined;
-if (process.env.NODE_ENV === "production") {
-  resolveApp = (relativePath) => path.resolve(__dirname, "./", relativePath);
-} else {
-  resolveApp = (relativePath) => path.resolve(__dirname, "../", relativePath);
-}
+// if (process.env.NODE_ENV === "production") {
+//   resolveApp = (relativePath) => path.resolve(__dirname, "./", relativePath);
+// } else {
+//   resolveApp = (relativePath) => path.resolve(__dirname, "../", relativePath);
+// }
+resolveApp = (relativePath) => path.resolve(__dirname, "../", relativePath);
 export default {
   packageJson: resolveApp("./package.json"),
   mainjs: resolveApp("./src/main.ts"),
